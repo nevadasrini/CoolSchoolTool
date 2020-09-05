c8a42ee1160e07c9c3e8535f30c26df2eebe0889
nevadasrini <58085004+nevadasrini@users.noreply.github.com>
2020-09-04 21:39:47 -0400

    can submit and view assignments (and delete them) whooooo!!!

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,8 @@ addForm.addEventListener('submit', (e) => {
 
     // get user info
     const subject = addForm['subject'].value;
-    const desc = addForm['desc'].value;
-    const docID = subject+desc;
+    const description = addForm['desc'].value;
+    const docID = subject+description;
 
     auth.onAuthStateChanged(user => {
         if (user) {
@@ -19,5 +19,52 @@ addForm.addEventListener('submit', (e) => {
                 userID: user.uid
             })
         }
+    })
+})
+
+const taskList = document.querySelector('#task-list');
+
+function renderTasks(doc){
+    //create elements
+
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let desc = document.createElement('span');
+    let cross = document.createElement('div');
+    let date = document.createElement('span');
+
+    //assign values to elements
+    li.setAttribute('data-id', doc.id)
+    name.textContent = doc.data().subject;
+    desc.textContent = doc.data().desc;
+    cross.textContent = 'X';
+    date.textContent = doc.data().duedate;
+
+    //append elements of taskList to each list item
+    li.appendChild(name);
+    li.appendChild(desc);
+    li.appendChild(cross);
+    li.appendChild(date);
+
+    //append list item to taskList
+    taskList.appendChild(li);
+
+    //deleting data
+    cross.addEventListener('click', (e) =>{
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('assignments').doc(id).delete();
+    })
+}
+
+//cycle through assignments and pass through render function
+db.collection('assignments').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        auth.onAuthStateChanged(user => {
+            if (doc.data().userID== user.uid){
+                renderTasks(doc)
+                console.log(doc.data());
+            }
+        })
     })
 })
